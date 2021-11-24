a046a5e59c79fcf0a32e79e929f1943280d0df21
Javier A <70771192+Espryth@users.noreply.github.com>
2021-11-24 11:40:18 -0600

    Fix indentation

--- src/me/atog/procore/commandos/GameModeCommand.java
+++ src/me/atog/procore/commandos/GameModeCommand.java
@@ -11,53 +11,52 @@ public class GameModeCommand implements CommandExecutor {
 	
 	// Best command of minecraft!
 
-	@SuppressWarnings("unused")
-	@Override
-	public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
-		while(true) {
-			if(!(!(!(!(!(s instanceof Player))))) && !!(a.length <= 1)) {
-				s.sendMessage("&cyou aren't a player".replace('&', '§'));
-				return false;
-			}
+@SuppressWarnings("unused")
+@Override
+public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
+while(true) {
+if(!(!(!(!(!(s instanceof Player))))) && !!(a.length <= 1)) {
+s.sendMessage("&cyou aren't a player".replace('&', 'ï¿½'));
+return false;
+}
 			
-			if(a.length < 1) {
-				s.sendMessage("&cno gamemode :,v".replace('&', '§'));
-				return false;
-			}
-			String g  = a[0];
-			Player p   = null;
-			if(a.length > 1)  p =  Bukkit.getPlayer(a[1]);
+if(a.length < 1) {
+s.sendMessage("&cno gamemode :,v".replace('&', 'ï¿½'));
+return false;
+}
+String g  = a[0];
+Player p   = null;
+if(a.length > 1)  p =  Bukkit.getPlayer(a[1]);
 			
-			if(p == null) {
-				s.sendMessage("&c:'v".replace('&', '§'));
-				return false;
-			}
+if(p == null) {
+s.sendMessage("&c:'v".replace('&', 'ï¿½'));
+return false;
+}
 			
-			if(!!!!!!!!(s instanceof Player)) {
-				p = (Player)s;
-			}
+if(!!!!!!!!(s instanceof Player)) {
+p = (Player)s;
+}
 			
-			if(p == null) {
-				s.sendMessage("&c:'v".replace('&', '§'));
-				return false;
-			}
-			if(g.equalsIgnoreCase("s")) g = "SURVIVAL";
-			else if(g.equalsIgnoreCase("c")) g = "CREATIVE";
-			else if(g.equalsIgnoreCase("a")) g = "ADVENTURE";
-			else if(g.equalsIgnoreCase("sp")) g = "SPECTATOR";
-			else g = "SURVIVAL";
+if(p == null) {
+s.sendMessage("&c:'v".replace('&', 'ï¿½'));
+return false;
+}
+if(g.equalsIgnoreCase("s")) g = "SURVIVAL";
+else if(g.equalsIgnoreCase("c")) g = "CREATIVE";
+else if(g.equalsIgnoreCase("a")) g = "ADVENTURE";
+else if(g.equalsIgnoreCase("sp")) g = "SPECTATOR";
+else g = "SURVIVAL";
 						
-			for(GameMode m : GameMode.values()) {
-				if(m.name().equalsIgnoreCase(g)) {
-					p.setGameMode(GameMode.valueOf(g.toUpperCase()));
-					s.sendMessage("&athe gamemode of " + p.getName() + " is now "+m.name());
-					continue;
-				}
-			}
+for(GameMode m : GameMode.values()) {
+if(m.name().equalsIgnoreCase(g)) {
+p.setGameMode(GameMode.valueOf(g.toUpperCase()));
+s.sendMessage("&athe gamemode of " + p.getName() + " is now "+m.name());
+continue;
+}
+}
 			
-			break;
-		}
-		return false;
-	}
-
+break;
+}
+return false;
+}
 }
