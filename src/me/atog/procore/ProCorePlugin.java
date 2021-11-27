46f9a988c55559c133a129efa22568fc03f66353
Jonakls <asmot54@hotmail.com>
2021-11-27 08:40:06 -0500

    @NOTNULL

--- src/me/atog/procore/ProCorePlugin.java
+++ src/me/atog/procore/ProCorePlugin.java
@@ -1,21 +1,26 @@
 package me.atog.procore;
 
+import com.sun.istack.internal.NotNull;
 import me.atog.procore.commandos.PluginCommandFlyMode;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.atog.procore.commandos.GameModeCommand;
 
 public class ProCorePlugin extends JavaPlugin {
-	
+
+	@Override
+	@NotNull
 	public final void onEnable() {
 		ProCorePlugin2.onEnable(this);
 	}
-	
+
+	@Override
+	@NotNull
 	public final void onDisable() {
 		ProCorePlugin2.onDisable(this);
 	}
-	
-	public final void registerCommandos() {
+
+	@NotNull public final void registerCommandos() {
 		getCommand("gm").setExecutor(new GameModeCommand());
 		getCommand("fly").setExecutor(new PluginCommandFlyMode());
 	}
